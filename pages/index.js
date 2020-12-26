a227faf8820644452d6c7207268b58f978ccbb49
Mattias Rost <rrostt@gmail.com>
2020-12-26 12:05:02 +0100

    Update. Make anim.

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,95 @@
-import Head from 'next/head'
+import { useEffect, useState, useCallback } from 'react'
 import styles from '../styles/Home.module.css'
+import dayjs from 'dayjs'
+import isBetween from 'dayjs/plugin/isBetween'
+import utc from 'dayjs/plugin/utc'
+dayjs.extend(isBetween)
+dayjs.extend(utc)
+
+const imagesUrl = 'https://l05c34qtnh.execute-api.eu-north-1.amazonaws.com/dev/images'
+
+const useImages = ({ from, to }) => {
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    fetch(`${imagesUrl}?from=${from}&to=${to}`)
+      .then(response => response.json())
+      .then(images => setImages(images))
+  }, [])
+
+  return images
+}
+
+const getHours = ({ from, to }) => {
+  let start = dayjs(from).startOf('hour')
+  const hours = []
+  do {
+    hours.push({ start, end: start.endOf('hour') })
+    start = start.add(1, 'hour')
+  } while (start.isBefore(dayjs(to)))
+  return hours
+}
+
+const ImageRow = ({ start, end, images }) =>
+  <div>
+    {dayjs(start).format('HH')}
+    {images.map(({ url }) => <img style={{ width: 120 }} src={url} />)}
+  </div>
+
+const Anim = ({ images }) => {
+  const [frame, setFrame] = useState(0)
+  const [playing, setPlaying] = useState(true)
+
+  const nextFrame = useCallback(() => {
+    if (images.length > 0) {
+      setFrame(frame => (frame + 1) % images.length)
+    }
+  }, [images])
+
+  const onChange = useCallback(e => {
+    setPlaying(false)
+    setFrame(e.target.value)
+  }, [])
+
+  const onClick = useCallback(e => {
+    setPlaying(playing => !playing)
+  })
+
+  useEffect(() => {
+    if (playing) {
+      const interval = setInterval(nextFrame, 200)
+      return () => clearInterval(interval)
+    }
+  }, [playing, images, nextFrame])
+
+  return images.length > 0 && <div style={{ maxWidth: 800, width: '100%' }}>
+    <div onClick={onClick}>
+      <img style={{ maxHeight: 800, display: 'block', margin: '0 auto' }} src={images[frame % images.length].url} />
+    </div>
+    <div>{images[frame % images.length].time}</div>
+    <input style={{ width: '100%' }} type='range' min={0} max={images.length - 1} value={frame} onChange={onChange} /><br />
+    <div>
+      {images.map(({ url }) => <img style={{ display: 'inline-block', width: `${100 / images.length}%`, height: 40 }} src={url} />)}
+    </div>
+  </div>
+}
 
 export default function Home() {
+  const from = '2020-12-24' // dayjs.utc().startOf('day').subtract(2, 'day').format() // '2020-12-23'
+  const to = dayjs.utc().endOf('day').format() // '2020-12-24'
+
+  const images = useImages({ from, to })
+  const hours = getHours({ from, to })
+    .map(({ start, end }) => ({
+      start,
+      end,
+      images: images.filter(({ time }) => dayjs(time).isBetween(start, end))
+    }))
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Anim images={images} />
+      {/* { hours.map(({ start, images }) => <ImageRow start={start} images={images} />)} */}
     </div>
   )
 }
