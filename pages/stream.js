5f2bc8f592aa4759e601e1802b89bccb0579593b
Mattias Rost <rrostt@gmail.com>
2021-02-09 15:36:35 +0100

    Use search param from window.location

--- pages/stream.js
+++ pages/stream.js
@@ -1,7 +1,17 @@
 import Stream from '../components/Stream'
 
+const getSearchParam = name => {
+  if (typeof window === 'undefined') {
+    return null
+  }
+
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name)
+}
+
 const StreamPage = ({ query }) => {
-  const { id } = query
+  // const { id } = query
+  const id = getSearchParam('id') || query.id
 
   console.log({id })
 
